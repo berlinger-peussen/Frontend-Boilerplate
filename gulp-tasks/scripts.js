bf18db0297f8d149fcbd352124b7d0dfedfe200c
Wieteke Öztürk <wieteke.ozturk@harperjones.nl>
2017-03-15 20:40:02 +0100

    return is implied...

--- gulp-tasks/scripts.js
+++ gulp-tasks/scripts.js
@@ -1,59 +1,56 @@
 module.exports = (config, gulp, argv, browserSync, lazypipe, plugins, isEnabled) => {
 
-const concat = plugins.concat,
-      merged = require('merge-stream')(),
- 			manifest = require('asset-builder')('./assets/manifest.json');
+  const concat = plugins.concat,
+        merged = require('merge-stream')(),
+        manifest = require('asset-builder')('./assets/manifest.json');
 
-const jsTasks = (filename) => {
-  return lazypipe()
-    .pipe( () => {
-      return plugins.if(isEnabled.maps, plugins.sourcemaps.init());
-    })
-    .pipe( () => {
-      return plugins.if(manifest.getProjectGlobs().js, plugins.babel({presets: ["es2015"]}));
-    })
-    .pipe(concat, filename)
-    .pipe(plugins.uglify, {
-      compress: {
-        'drop_debugger': isEnabled.stripJSDebug
-      }
-    })
-    .pipe( () => {
-      return plugins.if(isEnabled.rev, plugins.rev());
-    })
-    .pipe( () => {
-      return plugins.if(isEnabled.maps, plugins.sourcemaps.write('.', {
-        sourceRoot: 'assets/scripts/'
-      }));
-    })();
-};
-const writeToManifest = (directory) => {
-  return lazypipe()
-    .pipe(gulp.dest, config.path.dist + directory)
-    .pipe(browserSync.stream, {match: '**/*.js'})
-    .pipe(plugins.rev.manifest, config.path.dist + 'assets.json', {
-      base: config.path.dist,
-      merge: true
-    })
-    .pipe(gulp.dest, config.path.dist)();
-};
-gulp.task('jshint', () => {
-  return gulp.src([
-    'bower.json', 'gulpfile.js'
-  ].concat(manifest.getProjectGlobs().js))
-    .pipe(plugins.jshint())
-    .pipe(plugins.jshint.reporter('jshint-stylish'))
-    .pipe(plugins.if(isEnabled.failJSHint, plugins.jshint.reporter('fail')));
-});
-gulp.task('scripts', ['jshint'], () => {
-  manifest.forEachDependency('js', (dep) => {
-    merged.add(
-      gulp.src(dep.globs, {base: 'scripts'})
-        .pipe(plugins.plumber({errorHandler: plugins.notify.onError("Error: <%= error.message %>")}))
-        .pipe(jsTasks(dep.name))
-    );
+  const jsTasks = (filename) => {
+     lazypipe()
+      .pipe( () => {
+         plugins.if(isEnabled.maps, plugins.sourcemaps.init());
+      })
+      .pipe(concat, filename)
+      .pipe(plugins.uglify, {
+        compress: {
+          'drop_debugger': isEnabled.stripJSDebug
+        }
+      })
+      .pipe(() => {
+         plugins.if(isEnabled.rev, plugins.rev());
+      })
+      .pipe( () => {
+         plugins.if(isEnabled.maps, plugins.sourcemaps.write('.', {
+          sourceRoot: 'assets/scripts/'
+        }));
+      })();
+  };
+  const writeToManifest = (directory) => {
+     lazypipe()
+      .pipe(gulp.dest, config.path.dist + directory)
+      .pipe(browserSync.stream, {match: '**/*.js'})
+      .pipe(plugins.rev.manifest, config.path.dist + 'assets.json', {
+        base: config.path.dist,
+        merge: true
+      })
+      .pipe(gulp.dest, config.path.dist)();
+  };
+  gulp.task('jshint', () => {
+     gulp.src([
+      'bower.json', 'gulpfile.js'
+    ].concat(manifest.getProjectGlobs().js))
+      .pipe(plugins.jshint())
+      .pipe(plugins.jshint.reporter('jshint-stylish'))
+      .pipe(plugins.if(isEnabled.failJSHint, plugins.jshint.reporter('fail')));
   });
-  return merged
-    .pipe(writeToManifest('scripts'));
-});
+  gulp.task('scripts', ['jshint'], () => {
+    manifest.forEachDependency('js', (dep) => {
+      merged.add(
+        gulp.src(dep.globs, {base: 'scripts'})
+          .pipe(plugins.plumber({errorHandler: plugins.notify.onError("Error: <%= error.message %>")}))
+          .pipe(jsTasks(dep.name))
+      );
+    });
+    merged
+      .pipe(writeToManifest('scripts'));
+  });
 }
--- gulp-tasks/styles.js
+++ gulp-tasks/styles.js
@@ -4,15 +4,15 @@ module.exports = (config, gulp, argv, browserSync, lazypipe, plugins, isEnabled)
 		    manifest = require('asset-builder')('./assets/manifest.json');
 
 	const cssTasks = (filename) => {
-	  return lazypipe()
+	  lazypipe()
 	    .pipe( () => {
-	      return plugins.if(!isEnabled.failOnStyle, plugins.plumber({errorHandler: plugins.notify.onError("Error: <%= error.message %>")}));
+	      plugins.if(!isEnabled.failOnStyle, plugins.plumber({errorHandler: plugins.notify.onError("Error: <%= error.message %>")}));
 	    })
 	    .pipe( () => {
-	      return plugins.if(isEnabled.maps, plugins.sourcemaps.init());
+	      plugins.if(isEnabled.maps, plugins.sourcemaps.init());
 	    })
 	    .pipe( () => {
-	      return plugins.if('*.scss', plugins.sass({
+	      plugins.if('*.scss', plugins.sass({
 	        outputStyle: 'nested', // libsass doesn't support expanded yet
 	        precision: 10,
 	        includePaths: ['.'],
@@ -29,17 +29,17 @@ module.exports = (config, gulp, argv, browserSync, lazypipe, plugins, isEnabled)
 	    })
 	    .pipe(plugins.cssnano, { safe: true })
 	    .pipe( () => {
-	      return plugins.if(isEnabled.revisioning, plugins.rev());
+	      plugins.if(isEnabled.revisioning, plugins.rev());
 	    })
 	    .pipe( () => {
-	      return plugins.if(isEnabled.maps, plugins.sourcemaps.write('.', {
+	      plugins.if(isEnabled.maps, plugins.sourcemaps.write('.', {
 	        sourceRoot: 'assets/styles/'
 	      }));
 	    })();
 	};	
 
 	const writeToManifest = (directory) => {
-	  return lazypipe()
+	  lazypipe()
 	    .pipe(gulp.dest, config.path.dist + directory)
 	    .pipe(browserSync.stream, {match: '**/*.css'})
 	    .pipe(plugins.rev.manifest, config.path.dist + 'assets.json', {
@@ -59,7 +59,7 @@ module.exports = (config, gulp, argv, browserSync, lazypipe, plugins, isEnabled)
 
 	gulp.task('wiredep', function() {
 	  var wiredep = require('wiredep').stream;
-	  return gulp.src(manifest.getProjectGlobs().css)
+	  gulp.src(manifest.getProjectGlobs().css)
 	    .pipe(wiredep())
 	    .pipe(plugins.changed(config.path.source + 'styles', {
 	      hasChanged: plugins.changed.compareSha1Digest
@@ -71,17 +71,11 @@ module.exports = (config, gulp, argv, browserSync, lazypipe, plugins, isEnabled)
 
 	  manifest.forEachDependency('css', (dep) => {
 	    const cssTasksInstance = cssTasks(dep.name);
-	    if (!isEnabled.failOnStyle) {
-	      cssTasksInstance.on('error', (err) => {
-	        console.error(err.message);
-	        this.emit('end');
-	      });
-	    }
 	    merged.add(gulp.src(dep.globs, {base: 'styles'})
 	      .pipe(plugins.plumber({errorHandler: plugins.notify.onError("Error: <%= error.message %>")}))
 	      .pipe(cssTasksInstance));
 	  });
-	  return merged
+	  merged
 	    .pipe(writeToManifest('styles'));
 	});
 
